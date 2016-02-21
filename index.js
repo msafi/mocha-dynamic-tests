9763b313b8f0ad83c516bb23a9752b53e07816ea
M.K. Safi <msafi@msafi.com>
2016-02-20 21:25:42 -0500

    Allow all tests to be skipped or only'ed

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-module.exports = function(arrayOfTests, generateTestDetails) {
+module.exports = function(arrayOfTests, generateTestDetails, options) {
   arrayOfTests = arrayOfTests || []
   generateTestDetails = generateTestDetails || function() {}
+  options = options || {}
 
-  arrayOfTests.forEach(function(test) {
-    test = test || {}
+  var testsName = 'Dynamic tests'
+  var runTests = function() {
+    arrayOfTests.forEach(function (test) {
+      test = test || {}
 
-    var testDetails = generateTestDetails(test) || {}
+      var testDetails = generateTestDetails(test) || {}
 
-    if (test.only === true) {
-      it.only.call(null, testDetails.description, testDetails.body)
-    } else if (test.skip === true) {
-      it.skip.call(null, testDetails.description, testDetails.body)
-    } else {
-      it.call(null, testDetails.description, testDetails.body)
-    }
-  })
+      if (test.only === true) {
+        it.only.call(null, testDetails.description, testDetails.body)
+      } else if (test.skip === true) {
+        it.skip.call(null, testDetails.description, testDetails.body)
+      } else {
+        it.call(null, testDetails.description, testDetails.body)
+      }
+    })
+  }
+
+  if (options.only === true) {
+    describe.only(testsName, function() {
+      runTests()
+    })
+  } else if (options.skip === true) {
+    describe.skip(testsName, function() {
+      runTests()
+    })
+  } else {
+    describe(testsName, function() {
+      runTests()
+    })
+  }
 }
